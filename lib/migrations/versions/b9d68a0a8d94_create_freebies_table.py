187737e8683431363371e433094ef7e2e23646ca
cypriankanda <cyprianboit@gmail.com>
2025-05-26 00:33:07 +0300

    done

--- lib/migrations/versions/b9d68a0a8d94_create_freebies_table.py
+++ lib/migrations/versions/b9d68a0a8d94_create_freebies_table.py
@@ -17,8 +17,15 @@ depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        'freebies',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('item_name', sa.String, nullable=False),
+        sa.Column('value', sa.Integer, nullable=False),
+        sa.Column('dev_id', sa.Integer, sa.ForeignKey('devs.id'), nullable=False),
+        sa.Column('company_id', sa.Integer, sa.ForeignKey('companies.id'), nullable=False)
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table('freebies')
